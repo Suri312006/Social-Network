fd7abe1d35e1c2ae1bc45d77791236c7513831ce
suri312006 <suri312006@gmail.com>
2023-09-06 00:18:15 -0700

    can post from feed! part 3 done!

--- wey_backend/post/api.py
+++ wey_backend/post/api.py
@@ -2,7 +2,7 @@
 from django.http import JsonResponse
 # Create your views here.
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
+from .forms import PostForm
 from .models import Post
 from .serializers import PostSerializer
 
@@ -14,3 +14,19 @@ def post_list(request):
     
     return JsonResponse(serializer.data, safe=False)
     
+
+@api_view(['POST'])
+def post_create(request):
+    form = PostForm(request.data)
+    
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+        
+        serializer = PostSerializer(post)
+        
+        return JsonResponse(serializer.data, safe=False)
+    else:
+    
+        return JsonResponse({'error': 'fuck you'})
--- wey_backend/post/urls.py
+++ wey_backend/post/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import api
 
 urlpatterns = [
-    path('', api.post_list, name='post list')
+    path('', api.post_list, name='post list'),
+    path('create/', api.post_create, name='post_create')
 ]
