ffcd98c947a24205cdbc6834b0daaf8a0ca67ea6
suri312006 <suri312006@gmail.com>
2023-09-06 17:29:23 -0700

    api returns user data on search

--- wey_backend/search/api.py
+++ wey_backend/search/api.py
@@ -1,14 +1,17 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
+from account.models import User
+from account.serializers import UserSerializer
 @api_view(['POST'])
 def search(request):
     data = request.data
     query = data['query']
     
-    print('query ', query)
+    #icontains is case insensitive
+    users = User.objects.filter(name__icontains=query)
+    users_serializer = UserSerializer(users, many=True)
     
-    return JsonResponse({'search_received': 'lol'})
     
+    return JsonResponse(users_serializer.data, safe=False)
     
